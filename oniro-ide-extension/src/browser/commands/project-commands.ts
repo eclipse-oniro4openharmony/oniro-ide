273a4f9c4826cf803702439b6ef7784a742ba192
Mark Sujew <mark.sujew@typefox.io>
2023-04-17 13:57:08 +0000

    Add Language Configuration command to settings

--- oniro-ide-extension/src/browser/commands/project-commands.ts
+++ oniro-ide-extension/src/browser/commands/project-commands.ts
@@ -1,87 +1,88 @@
 import { Command, CommandContribution, CommandRegistry } from "@theia/core";
 import { injectable } from "@theia/core/shared/inversify";
 
-const PROJECT_CATEGORY = 'project'
+const PROJECT_CATEGORY = 'Project';
+const PROJECT_CATEGORY_KEY = 'oniro/projectMenu/project';
 // project commands
 export const NEW_PROJECT_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.new-project',
     label: 'New Project',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/newProject');
+}, 'oniro/projectMenu/newProject', PROJECT_CATEGORY_KEY);
 
 export const NEW_MULTI_WORKSPACE_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.new-multi-project-workspace',
     label: 'new Multi-Project Worksapce...',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/newMultiProjectWorkspace');
+}, 'oniro/projectMenu/newMultiProjectWorkspace', PROJECT_CATEGORY_KEY);
 
 export const OPEN_PROJECT_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.open-project',
     label: 'Open Project',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/openProject');
+}, 'oniro/projectMenu/openProject', PROJECT_CATEGORY_KEY);
 
 export const CLOSE_PROJECT_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.close-project',
     label: 'Close Project',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/closeProject');
+}, 'oniro/projectMenu/closeProject', PROJECT_CATEGORY_KEY);
 
 // build commands
 export const CLEAN_TARGETS_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.clean-targets',
     label: 'Clean Targets',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/cleanTargets');
+}, 'oniro/projectMenu/cleanTargets', PROJECT_CATEGORY_KEY);
 
 export const BUILD_TARGET_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.build-target',
     label: 'Build Targets',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/buildTargets');
+}, 'oniro/projectMenu/buildTargets', PROJECT_CATEGORY_KEY);
 
 export const REBUILD_TARGET_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.rebuild-targets',
     label: 'Rebuild all target files',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/rebuildTargets');
+}, 'oniro/projectMenu/rebuildTargets', PROJECT_CATEGORY_KEY);
 
 export const BATCH_BUILD_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.batch-build',
     label: 'Batch build...',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/batchBuild');
+}, 'oniro/projectMenu/batchBuild', PROJECT_CATEGORY_KEY);
 
 export const TRANSLATE_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.translate-project',
     label: 'Translate...',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/translate');
+}, 'oniro/projectMenu/translate', PROJECT_CATEGORY_KEY);
 
 export const STOP_BUILD_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.stop-build',
     label: 'Stop build',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/stopBuild');
+}, 'oniro/projectMenu/stopBuild', PROJECT_CATEGORY_KEY);
 
 // project config commands
 export const SELECT_DEVICE_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.device-select',
     label: 'Select Device...',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/selectDevice');
+}, 'oniro/projectMenu/selectDevice', PROJECT_CATEGORY_KEY);
 
 export const REMOVE_ITEM_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.remove-project-item',
     label: 'Remove Item',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/removeItem');
+}, 'oniro/projectMenu/removeItem', PROJECT_CATEGORY_KEY);
 
 export const TARGET_OPTIONS_COMMAND = Command.toLocalizedCommand({
     id: 'oniro.target-options',
     label: 'Options...',
     category: PROJECT_CATEGORY
-}, 'oniro/projectMenu/targetOptions');
+}, 'oniro/projectMenu/targetOptions', PROJECT_CATEGORY_KEY);
 
 @injectable()
 export class ProjectCommandContribution implements CommandContribution {
--- oniro-ide-extension/src/browser/oniro-ide-frontend-module.ts
+++ oniro-ide-extension/src/browser/oniro-ide-frontend-module.ts
@@ -1,17 +1,12 @@
-/**
- * Generated using theia-extension-generator
- */
-import { OniroIdeCommandContribution, OniroIdeMenuContribution } from './oniro-ide-contribution';
 import { CommandContribution, MenuContribution } from '@theia/core/lib/common';
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { ProjectMenuContribution } from './menus/project-menu';
 import { ProjectCommandContribution } from './commands/project-commands';
+import { LocalizationMenuContribution } from './menus/localization-menu';
 
 export default new ContainerModule(bind => {
-    // add your contribution bindings here
-    bind(CommandContribution).to(OniroIdeCommandContribution);
-    bind(MenuContribution).to(OniroIdeMenuContribution);
 
     bind(CommandContribution).to(ProjectCommandContribution).inSingletonScope();
     bind(MenuContribution).to(ProjectMenuContribution).inSingletonScope();
+    bind(MenuContribution).to(LocalizationMenuContribution).inSingletonScope();
 });

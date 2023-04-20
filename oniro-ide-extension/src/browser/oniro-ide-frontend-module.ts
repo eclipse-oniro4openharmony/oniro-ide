02975897367eb8b9465a0bda5ea9c82d32099059
Jonah Iden <jonah.iden@typefox.io>
2023-04-20 14:06:27 +0200

    Basic wizard framework and new project wizard implementation (#37)
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- oniro-ide-extension/src/browser/oniro-ide-frontend-module.ts
+++ oniro-ide-extension/src/browser/oniro-ide-frontend-module.ts
@@ -4,9 +4,16 @@ import { ProjectMenuContribution } from './menus/project-menu';
 import { ProjectCommandContribution } from './commands/project-commands';
 import { LocalizationMenuContribution } from './menus/localization-menu';
 
+import { NewProjectWizardFactory, createNewProjectWizardContainer } from './wizards/new-project/new-project-wizard';
+import { WizardDialog } from './wizards/wizard-dialog';
+import { NewProjectConfig, ProjectCreationService } from './services/project-creation-service';
+
 export default new ContainerModule(bind => {
+    bind(MenuContribution).to(LocalizationMenuContribution).inSingletonScope();
 
     bind(CommandContribution).to(ProjectCommandContribution).inSingletonScope();
     bind(MenuContribution).to(ProjectMenuContribution).inSingletonScope();
-    bind(MenuContribution).to(LocalizationMenuContribution).inSingletonScope();
+
+    bind(ProjectCreationService).toSelf().inSingletonScope();
+    bind(NewProjectWizardFactory).toFactory(ctx => () => createNewProjectWizardContainer(ctx.container).get(WizardDialog<NewProjectConfig>));
 });

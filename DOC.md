942127cf3aabe25dfbd29d2529f90cdccdbd74ab
Jan Bicker <jan.bicker@typefox.io>
2023-05-08 11:32:15 +0200

    Theia to VSCode communication - simple example, initial Doc
    Small example that shows how to add a command which calls a command
    inside a vscode extension
    and how to add that to the toolbar.
    Additionally a first initial documentation got added.
    
    Update DOC.md
    
    Co-authored-by: Jonah Iden <jonah.iden@typefox.io>
    
    Update DOC.md
    
    Co-authored-by: Jonah Iden <jonah.iden@typefox.io>
    
    Update DOC.md
    
    Co-authored-by: Jonah Iden <jonah.iden@typefox.io>
    
    Adapted text

---
+++ DOC.md
@@ -0,0 +1,23 @@
+# Documentation
+## Requirement 1: Definition of the user interface and flow improvements
+### Design documentation
+#### Customising the Toolbar
+--
+
+## Requirement 4: VS Code API usage & Theia extension - VS Code extension communication
+### Design documentation
+#### Communication between theia and VScodeExtension
+To enable communication between Eclipse Theia and VSCode extensions, Theia's `CommandRegistry` can be used. It allows extensions to register and invoke commands. All commands defined in a VSCode Extension are registered in the `CommandRegistry` after the extension has been loaded. 
+To invoke such a command implemented by a VSCode extension, Eclipse Theia extensions can use the executeCommand method in the command registry which looks like this: `executeCommand<T>(commandId: string, ...args: any[]): Promise<T | undefined>`. 
+
+It's important to note that if the VSCode extension that implements the command is not installed, or if the command ID is incorrect, an error will be thrown. Therefore, it's good practice to catch and handle these errors.
+
+**Example:**
+To try a simple example one can install the notification-sample-vsix into the Oniro theia instance with the `Extensions: Install from VSIX...` command. 
+The vsix can be found here: /huawei-ide/example-assets/notifications-sample-0.0.1.vsix 
+and is generated from https://github.com/microsoft/vscode-extension-samples/tree/main/notifications-sample.
+
+Every command registered in this extension is callable in theia simply by its id.  
+This is implemented in /oniro-ide-extension/src/browser/toolbar/manager/oniro-toolbar-commands.ts.
+There the example-command is registered and executes the 'notifications-sample.showInfo' command from the VSCode extension.
+Furthermore one can see in the same file how it is added to the toolbar manager: the command needs to be added to the array which is returned by the method getOniroCommands().
--- oniro-ide-extension/src/browser/toolbar/manager/oniro-toolbar-commands.ts
+++ oniro-ide-extension/src/browser/toolbar/manager/oniro-toolbar-commands.ts
@@ -3,18 +3,35 @@ import { inject, injectable } from '@theia/core/shared/inversify';
 import { CommonCommands } from '@theia/core/lib/browser/common-frontend-contribution';
 import { EditorCommands } from '@theia/editor/lib/browser';
 import { WorkspaceCommands } from '@theia/workspace/lib/browser/workspace-commands';
+import { BUILD_CATEGORY, PROJECT_CATEGORY } from '../../commands/project-commands';
 
 @injectable()
 export class OniroToolbarCommands implements CommandContribution {
     @inject(CommandRegistry) commandRegistry: CommandRegistry;
-    registerCommands(registry: CommandRegistry): void {}
+    
+    registerCommands(registry: CommandRegistry): void {
+        // define a command...
+        const exampleCommand: Command = {
+            id: 'example-command',
+            label: 'Example Command',
+            iconClass: 'fa fa-star',
+            category: 'Example'
+        };
+        // ...and register it
+        registry.registerCommand(exampleCommand, {
+            // Let the execute method excute a command by id. 
+            // In this case an id provided by a vscode extension packaged and installed from this here:
+            // https://github.com/microsoft/vscode-extension-samples/tree/main/notifications-sample
+            execute: () => registry.executeCommand('notifications-sample.showInfo').catch((e: Error) => console.error(e.message))
+        });
+    }
     
     getOniroCommands(): Command[] {
         const commands = Array.from(this.commandRegistry.getAllCommands());
         
         // Filter by category and filter out commands that don't have an icon nor a label
-        const compileCommands = commands.filter(cmd => !!cmd.iconClass && !!cmd.label && cmd.category === 'Compile');
-        const projectCommands = commands.filter(cmd => !!cmd.iconClass && !!cmd.label && cmd.category === 'Project');
+        const compileCommands = commands.filter(cmd => !!cmd.iconClass && !!cmd.label && cmd.category === BUILD_CATEGORY);
+        const projectCommands = commands.filter(cmd => !!cmd.iconClass && !!cmd.label && cmd.category === PROJECT_CATEGORY);
         const debugCommands = commands.filter(cmd => !!cmd.iconClass && !!cmd.label && cmd.category === 'Debug');
         
         // Pick out the single commands that we want to add to the toolbar, add an icon and a category
@@ -29,6 +46,12 @@ export class OniroToolbarCommands implements CommandContribution {
             Object.assign(EditorCommands.GO_FORWARD, { iconClass: 'codicon codicon-arrow-right' }),
         ].map(cmd => Object.assign(cmd, { category: 'File' }));
 
-        return [...projectCommands, ...fileCommands, ...compileCommands, ...debugCommands];
+        // Pick commands by id and add it to the returned array 
+        const otherCommands: Command[] = [];
+        // Add the example command from above to the toolbar
+        const exampleCommand = this.commandRegistry.getCommand('example-command');
+        exampleCommand && otherCommands.push(exampleCommand);
+
+        return [...projectCommands, ...fileCommands, ...compileCommands, ...debugCommands, ...otherCommands];
     }
 }

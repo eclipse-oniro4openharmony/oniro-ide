fc2936410f1899af5d2cff47bf9317e981692cd5
Jan Bicker <jan.bicker@typefox.io>
2023-05-08 11:57:00 +0200

    Added defaults

--- oniro-ide-extension/src/browser/toolbar/oniro-toolbar-defaults.ts
+++ oniro-ide-extension/src/browser/toolbar/oniro-toolbar-defaults.ts
@@ -1,5 +1,7 @@
 import { DeflatedToolbarTree, ToolbarAlignment } from '@theia/toolbar/lib/browser/toolbar-interfaces';
 import { OniroToolbarSideContribution } from './layout/oniro-toolbar-side-contribution';
+import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
+import { FILE_NAVIGATOR_TOGGLE_COMMAND_ID } from '@theia/navigator/lib/browser/navigator-contribution';
 
 // This file specifies the default layout of the toolbar. This binding should be overridden for extenders.
 // Both Toolbar Command Items and Toolbar Contributions can be specified here.
@@ -8,6 +10,20 @@ export const OniroToolbarDefaults: () => DeflatedToolbarTree = () => ({
         [ToolbarAlignment.LEFT]: [],
         [ToolbarAlignment.CENTER]: [[]],
         [ToolbarAlignment.RIGHT]: [
+            [
+                {
+                    id: GettingStartedCommand.id,
+                    command: GettingStartedCommand.id,
+                    tooltip: 'Getting Started',
+                    icon: 'codicon codicon-home',
+                },
+                {
+                    id: FILE_NAVIGATOR_TOGGLE_COMMAND_ID,
+                    command: FILE_NAVIGATOR_TOGGLE_COMMAND_ID,
+                    tooltip: 'Explorer',
+                    icon: 'codicon codicon-files'
+                }
+            ],
             [
                 {
                     id: OniroToolbarSideContribution.ID,
